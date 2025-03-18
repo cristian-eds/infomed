d5389ef3bdf57916b3d2a2015b5eb563a36c4f57
Cristian <cristian.santos0308@gmail.com>
2025-03-18 17:43:06 -0300

    Enable localhost in cors configuration

--- src/main/java/io/github/cristian_eds/InfoMed/controller/config/WebConfig.java
+++ src/main/java/io/github/cristian_eds/InfoMed/controller/config/WebConfig.java
@@ -3,6 +3,7 @@ package io.github.cristian_eds.InfoMed.controller.config;
 import io.github.cristian_eds.InfoMed.interceptors.HistoricInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(historicInterceptor)
                 .addPathPatterns("/medicine/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173","*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
