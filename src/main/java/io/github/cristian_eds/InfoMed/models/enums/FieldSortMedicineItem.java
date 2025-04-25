a1d42a053719380351979b7a7949580688a25507
Cristian <cristian.santos0308@gmail.com>
2025-04-25 15:21:10 -0300

    Created enums to control medicine items sort

---
+++ src/main/java/io/github/cristian_eds/InfoMed/models/enums/FieldSortMedicineItem.java
@@ -0,0 +1,20 @@
+package io.github.cristian_eds.InfoMed.models.enums;
+
+public enum FieldSortMedicineItem {
+
+    NAME("m.name"),
+    NUMBER("mi.medicineItemSequence"),
+    FREQUENCE("m.frequencyHours"),
+    DAY_HOUR("mi.dayHour"),
+    CONCLUSION("mi.conclusion");
+
+    private final String decription;
+
+    FieldSortMedicineItem(String description) {
+        this.decription = description;
+    }
+
+    public String getDescription() {
+        return this.decription;
+    }
+}
---
+++ src/main/java/io/github/cristian_eds/InfoMed/models/enums/TypeSortMedicineItem.java
@@ -0,0 +1,6 @@
+package io.github.cristian_eds.InfoMed.models.enums;
+
+public enum TypeSortMedicineItem {
+
+    DESC, ASC;
+}
